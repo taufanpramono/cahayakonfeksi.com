945ed67e505cf3dabb536bd4b915879db1dae14f
Mohammad Taufan Pramono <pramonotaufan@gmail.com>
2023-08-28 20:34:16 +0700

    Latest Version

---
+++ uploads/aios/firewall-rules/settings.php
@@ -0,0 +1,6 @@
+<?php __halt_compiler();
+/**
+ * This file was created by All In One Security (AIOS) plugin.
+ * The file is required for storing and retrieving your firewall's settings.
+ */
+{"aios_enable_rename_login_page":"1","aios_login_page_slug":"backpanel","aios_enable_brute_force_attack_prevention":"","aios_brute_force_secret_word":"","aios_cookie_based_brute_force_redirect_url":"http:\/\/127.0.0.1","aios_brute_force_attack_prevention_pw_protected_exception":"","aios_brute_force_attack_prevention_ajax_exception":"","aios_brute_force_secret_cookie_name":"aios_brute_force_secret_e8d8502b200bba0cf6e38677fe17bbb6","aios_brute_force_cookie_salt":")Bp(9kE&*%{,#p9}4;Wk3_Xt~Ps\/ZNpwCYuv ifQ#A\/^S;KV50lu2cmN_`h+$6%u;cCa?)1#k_~!S$3_w=V]Z^El9MWRpJAy#L<(%_VAxH$i+>$0PD<C%:+[8YIRC=Q{","aiowps_blacklist_user_agents":[],"aios_ip_retrieve_method":0,"aiowps_ip_retrieve_method":0,"aiowps_blacklist_ips":[]}
